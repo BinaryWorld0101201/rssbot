d95e8b80c5bb7bb5f1450a477bc42fe3d11fe953
nierunjie <nierunjie@gmail.com>
2018-11-20 18:09:58 +0800

    update

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ logging.basicConfig(level=logging.ERROR,
                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
 if __name__ == '__main__':
+    logging.info('rssbot started')
     token = sys.argv[1]
     bot = RSSBot(token)
     bot.run()
