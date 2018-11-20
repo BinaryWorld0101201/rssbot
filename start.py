832706652a62d170454981daf9a94e80d9a7ecbe
nierunjie <nierunjie@gmail.com>
2018-11-20 19:49:20 +0800

    update

--- start.py
+++ start.py
@@ -1,9 +1,7 @@
 def start(bot, update):
     chat_id = update.message.chat_id
     text = '欢迎使用，玩的开心\n'
-    text += '/rss 查看\n'
-    text += '/sub 订阅\n'
-    text += '/unsub 退订\n'
+    text += '/rss 查看；/sub 订阅；/unsub 退订\n'
     text += '源码：<a href="https://github.com/nierunjie/rssbot">Github</a>，疯狂暗示小星星\n'
     text += '作者：@Lanthora\n'
     bot.send_message(chat_id, text,
