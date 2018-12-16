b6ee1b7278fda420efd5b0f70fb7390db0326363
nierunjie <nierunjie@gmail.com>
2018-12-16 12:27:09 +0800

    update

--- start.py
+++ start.py
@@ -6,7 +6,7 @@ def start(bot, update):
     text += '/unsub 退订，使用方法同订阅\n'
     text += '/push 推荐到频道，使用方法同订阅\n\n'
     
-    text += '源码：<a href="https://github.com/nierunjie/rssbot">GitHub ⭐</a>\n'
+    text += '源码：<a href="https://github.com/nierunjie/rssbot">GitHub</a>\n'
     text += '频道：<a href="https://t.me/rssbotchannel">RSS精选</a>\n'
     bot.send_message(chat_id, text,
                      parse_mode='HTML',
